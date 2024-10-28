1090d4b8b422ff414b5d3e066f06230ce8a9aee7
imag6 <isabellahmagnusdottir@gmail.com>
2024-10-28 10:10:21 +0100

    Changed filename and added directed edges

--- project/simp_ass.py
+++ project/simp_ass.py
@@ -1,19 +1,19 @@
 from manim import *
 
-class CreateGraph(Scene):
+class SimplifyingAssumption1(Scene):
     def construct(self):
         vertices = [1,2,3,4,5,6]
         pos_edges = [(1,3), (2,3)]
         neg_edges = [(3,4), (3,5), (3,6)]
 
         lt = {1: [-1, 1, 0], 2: [-1, -1, 0], 3: [0, 0, 0], 4: [2, 1, 0], 5: [2, 0, 0], 6: [2, -1, 0]}
-        g = Graph(vertices, pos_edges+neg_edges, layout=lt,
+        g = DiGraph(vertices, pos_edges+neg_edges, layout=lt,
                   vertex_config={3: {"fill_color": RED}},
                   edge_config={(3, 4): {"stroke_color": RED},
                                (3, 5): {"stroke_color": RED},
-                               (3, 6): {"stroke_color": RED}})
+                               (3, 6): {"stroke_color": RED},
+                               "tip_config": {"tip_length": 0.2, "tip_width": 0.2}})
         self.play(Create(g))
-
         self.wait()
 
         self.play(
@@ -30,17 +30,16 @@ class CreateGraph(Scene):
         self.play(
             Create(g.add_edges(*new_edges)),
             [g.vertices[e].animate.shift(RIGHT * 1) for e in neg_cluster],
-            [g.edges[edge].animate.shift(RIGHT * 1) for edge in neg_edges],
-            Create(g.add_edges(*new_edges))
+            [g.edges[edge].animate.shift(RIGHT * 1) for edge in neg_edges]
         )
         self.wait()
 
-        new_neg_edge = [(7, 3)]
+        new_neg_edge = (7, 3)
         self.play(
             [FadeToColor(g.edges[edge], WHITE) for edge in neg_edges],
             FadeToColor(g.vertices[3], WHITE),
             FadeToColor(g.vertices[7], RED),
-            Create(g.add_edges(*new_neg_edge, edge_config={(7, 3): {"stroke_color": RED}}))
+            Create(g.add_edges(new_neg_edge, edge_config={(7, 3): {"stroke_color": RED}}))
         )
 
         self.wait()
