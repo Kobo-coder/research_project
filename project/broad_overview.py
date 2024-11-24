766b42efe61d1c7f339c077e3e15c9a7880a7ec6
imag6 <isabellahmagnusdottir@gmail.com>
2024-11-24 12:29:59 +0100

    Roughly done broad overview of the algorithm

--- project/broad_overview.py
+++ project/broad_overview.py
@@ -1,13 +1,16 @@
+import math
+
 from manim import *
 import random as rand
 from math import pow, log2, floor
-from manim.utils.color.XKCD import REDWINE
+
+from manim.utils.color.X11 import HOTPINK
 
 
 def generate_edge_config(neg_edges):
     edge_config = {}
     for e in neg_edges:
-        edge_config[e] = { "stroke_color": RED_E }
+        edge_config[e] = { "stroke_color": RED }
     return edge_config
 
 def is_position_valid(x, y, existing_positions, min_dist):
@@ -56,6 +59,8 @@ def determine_num_of_edges(vertex, layout, bounds, min_edges_per_vertex, max_edg
     else:
         return np.random.randint(min_edges_per_vertex, max_edges_per_vertex + 1)
 
+def distance(v1, v2, layout):
+    return np.sqrt(sum((a - b) ** 2 for a, b in zip(layout[v1], layout[v2])))
 
 def generate_edges(layout, bounds, min_edges_per_vertex=2, max_edges_per_vertex=6, max_edge_length=4.0):
     edges = []
@@ -63,11 +68,8 @@ def generate_edges(layout, bounds, min_edges_per_vertex=2, max_edges_per_vertex=
     pos_edges = []
     vertices = list(layout.keys())
 
-    def distance(v1, v2):
-        return np.sqrt(sum((a - b) ** 2 for a, b in zip(layout[v1], layout[v2])))
-
     for v in vertices:
-        distances = [(u, distance(v, u)) for u in vertices if u != v]
+        distances = [(u, distance(v, u, layout)) for u in vertices if u != v]
         distances.sort(key=lambda x: x[1])
 
         num_edges = determine_num_of_edges(v, layout, bounds, min_edges_per_vertex, max_edges_per_vertex)
@@ -76,7 +78,6 @@ def generate_edges(layout, bounds, min_edges_per_vertex=2, max_edges_per_vertex=
             if dist <= max_edge_length:
                 if len([(e1, e2) for e1, e2 in edges if (e1 == v and e2 == u) or (e1 == u and e2 == v)]) == 0:
                     edge = (u,v) if layout[u][0] < layout[v][0] else (v,u)
-                    #edge = (min(u,v), max(u,v))
                     edges.append(edge)
 
                     if rand.choices([0, 1], [0.75, 0.25], k=1)[0]:
@@ -99,7 +100,7 @@ def partition_graph(layout, edges, x_bound):
     return partitions
 
 
-def indicate_pass(self, graph, partitions):
+def indicate_pass(self, graph, newly_pos, partitions):
     wave_animations = []
     for partition in partitions:
         edge_copies = [
@@ -113,6 +114,7 @@ def indicate_pass(self, graph, partitions):
                 edge_cop.set_color(BLUE_D).set_stroke(width=4),
                 time_width=0.1
             ) for edge_cop in edge_copies],
+            [FadeToColor(graph.edges[e], PURE_RED) for e in newly_pos],
             lag_ratio=0
         )
         wave_animations.append(partition_animations)
@@ -125,8 +127,91 @@ def indicate_pass(self, graph, partitions):
         run_time=2
     )
 
+def find_most_mid_vertex(layout, deviation=0.25):
+    around_zero = []
+    for v, pos in layout.items():
+        if pos[0] < 0 and math.isclose(pos[1], 0, abs_tol=deviation):
+            around_zero.append(v)
+
+    if len(around_zero) == 0:
+        new_deviation = deviation + 0.1
+        find_most_mid_vertex(layout, new_deviation)
+
+    return min(around_zero, key=lambda v: layout[v][0])
+
 
 class BroadOverview(Scene):
+
+    def indicate_source(self, graph, layout):
+        source = find_most_mid_vertex(layout)
+        self.play(
+            FadeToColor(graph.vertices[source], GREEN),
+            Write(MathTex(r"\text{s}").next_to(graph.vertices[source], LEFT*0.5))
+        )
+        self.wait(5)
+
+        return source
+
+    def display_title(self):
+        title = Tex("Overview of the Algorithm")
+        title.scale(1)
+        h_line = Line(
+            start=title.get_left(),
+            end=title.get_right(),
+        ).next_to(title, DOWN, buff=0.1)
+
+        self.play(
+            Write(title, run_time=2, rate_func=slow_into),
+            Create(h_line, run_time=2, rate_func=slow_into),
+
+        )
+        self.wait(3)
+        self.play(
+            Unwrite(title, run_time=2, rate_func=slow_into),
+            Uncreate(h_line, run_time=2, rate_func=slow_into)
+        )
+        self.wait()
+
+    def run_dijkstra(self, source, graph, layout):
+        distances = {v: float("inf") for v in graph.vertices}
+        distances[source] = 0
+        visited = set()
+
+        while len(visited) < len(graph.vertices):
+            current = min(
+                (v for v in distances if v not in visited),
+                key=lambda x: distances[x]
+            )
+            self.play(FadeToColor(graph.vertices[current], YELLOW), run_time=0.005)
+
+            neighboring = [(v, 0) for (u, v) in graph.edges.keys() if u == current] + [(u, 1) for (u, v) in graph.edges.keys() if v == current]
+
+            for neighbor, is_cur_dest in neighboring:
+                if neighbor in visited:
+                    continue
+                edge_weight = abs(distance(current, neighbor, layout))
+                new_distance = distances[current] + edge_weight
+
+                if is_cur_dest:
+                    edge = graph.edges[(neighbor, current)]
+                else:
+                    edge = graph.edges[(current, neighbor)]
+
+                if new_distance < distances[neighbor]:
+                    distances[neighbor] = new_distance
+                cute_green = rgb_to_color([124, 228, 129])
+                self.play(ShowPassingFlash(edge.copy().set_color(cute_green).set_stroke(width=4).set_z_index(edge.z_index+1)),
+                        FadeToColor(edge, GREY), time_width=1, run_time=0.005)
+
+            visited.add(current)
+            if current != source:
+                self.play(FadeToColor(graph[current], cute_green), run_time=0.005)
+
+
+        self.wait()
+
+
+
     def construct(self):
         num_of_vertices = 150
         vertices = [v for v in range(1, num_of_vertices+1)]
@@ -136,45 +221,55 @@ class BroadOverview(Scene):
         edges, pos_edges, neg_edges = generate_edges(layout, bounds)
 
         graph = Graph(vertices, edges, edge_config=generate_edge_config(neg_edges), layout=layout)
+        [graph.vertices[vertex].set_z_index(graph.z_index + 1) for vertex in vertices]
+
+        self.display_title()
 
         self.play(Create(graph))
         self.wait()
 
-        self.play([Indicate(graph.edges[e], color=REDWINE) for e in neg_edges])
+        self.play([Indicate(graph.edges[e], color=PURE_RED) for e in neg_edges])
         self.wait()
 
         partitions = partition_graph(layout, edges, bounds[0]+1)
 
         past = None
-        for i in range(int(log2(num_of_vertices))):
-            num_in_pass = pow(len(neg_edges), 2/3)
+        num_of_rounds = 1
+
+        font_size = 26
+        rounds_text = Text("No. of elimination rounds: ", font_size=font_size).to_corner(LEFT + UP)
+        cur = Text(f"{num_of_rounds}", font_size=font_size).next_to(rounds_text)
+        text = VGroup(rounds_text, cur)
+
+        while len(neg_edges) > 0:
             newly_pos = []
-            for j in range(int(num_in_pass)):
-                if not neg_edges:
-                    break
+            to_be_eliminated = pow(len(neg_edges), 1 / 3)
 
-                to_be_eliminated = pow(len(neg_edges), 1 / 3)
-                rounds = Text("No. of elimination rounds: ", font_size=36).to_corner(LEFT+UP)
-                cur = Text(f"{j+1}", font_size=36).next_to(rounds)
-                text = VGroup(rounds, cur)
+            for e in neg_edges:
+                if rand.choices((0, 1), [(len(neg_edges) - to_be_eliminated) / len(neg_edges), to_be_eliminated], k=1)[0]:
+                    neg_edges.remove(e)
+                    pos_edges.append(e)
+                    newly_pos.append(e)
 
-                for e in neg_edges:
-                    if rand.choices((0, 1), [(len(neg_edges) - to_be_eliminated) / len(neg_edges), to_be_eliminated], k=1)[0]:
-                        neg_edges.remove(e)
-                        pos_edges.append(e)
-                        newly_pos.append(e)
+            if past is None:
+                anim = Write(text)
+            else:
+                anim = ReplacementTransform(past, cur)
 
-                if past is None:
-                    anim = Write(text)
-                else:
-                    anim = ReplacementTransform(past, cur)
+            self.play(anim)
+            indicate_pass(self, graph, newly_pos, partitions)
 
-                self.play(anim)
-                indicate_pass(self, graph, partitions)
+            self.play(
+                [FadeToColor(graph.edges[e], color=WHITE) for e in newly_pos],
+                run_time=1
+            )
+            self.wait()
+            past = cur
+            cur = Text(f"{num_of_rounds}", font_size=font_size).next_to(rounds_text)
+            num_of_rounds += 1
 
-                self.play(
-                    [FadeToColor(graph.edges[e], color=WHITE) for e in newly_pos],
-                    run_time=1
-                )
-                self.wait()
-                past = cur
+        self.play(Unwrite(text))
+        self.wait()
+
+        source = self.indicate_source(graph, layout)
+        self.run_dijkstra(source, graph, layout)
